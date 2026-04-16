b8989b384043ca9ce29d174c1bed42ebcadd9787
Camila <22200168@ue.edu.pe>
2026-04-15 21:22:21 -0500

    netlify confi

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,14 @@ export default defineConfig({
   server: {
     port: 5173,
     open: true
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
   }
 })
